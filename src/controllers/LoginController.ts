41ac62611d249c65effca06795352d078a126bed
jydnbach <jaydenkim95@gmail.com>
2024-07-27 07:52:58 +0900

    refactor: login controller

---
+++ src/controllers/LoginController.ts
@@ -0,0 +1,24 @@
+import { Router, Request, Response, NextFunction } from 'express';
+
+@controller('/')
+class LoginController {
+  @get('/login')
+  getLogin(req: Request, res: Response): void {
+    // req.session
+    if (req.session && req.session.loggedIn) {
+      res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+      `);
+    } else {
+      res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+      `);
+    }
+  }
+}
