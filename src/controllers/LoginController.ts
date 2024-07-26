8a0a7dd5dff5ff6eb8199d55a76a1487d8536663
jydnbach <jaydenkim95@gmail.com>
2024-07-27 08:10:05 +0900

    proof of concept errors fix

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,25 +1,23 @@
 import { get } from './decorators/routes';
 import { Router, Request, Response, NextFunction } from 'express';
+import { controller } from './decorators/controller';
 
-@controller('/')
+@controller('/auth')
 class LoginController {
   @get('/login')
-  getLogin(req: Request, res: Response): void {
-    // req.session
-    if (req.session && req.session.loggedIn) {
-      res.send(`
-      <div>
-        <div>You are logged in</div>
-        <a href="/logout">Logout</a>
-      </div>
+  getLogin(req: Request, res: Response) {
+    res.send(`
+      <form method="POST" >
+        <div>
+          <label>Email</label>
+          <input name="email"/>
+        </div>
+        <div>
+          <label>Password</label>
+          <input name="password" type="password" />
+        </div>
+        <button>Submit</button>
+      </form>
       `);
-    } else {
-      res.send(`
-      <div>
-        <div>You are not logged in</div>
-        <a href="/login">Login</a>
-      </div>
-      `);
-    }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 
+import './controllers/LoginController';
+import { router as controllerRouter } from './controllers/decorators/controller';
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['dfjksal'] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
